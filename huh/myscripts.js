60e4e1ae20f762ec703b9c64074432f44c0208e0
keegang6705 <75747783+keegang6705@users.noreply.github.com>
2022-04-25 12:34:28 +0700

    Update myscripts.js

--- huh/myscripts.js
+++ huh/myscripts.js
@@ -1,4 +1,4 @@
-const print=console.log;
+const print = console.log;
 var numberr = {"a":1,"b":2}
 function num(key,a){
     if (isNaN(numberr[key])){
@@ -8,3 +8,12 @@ function num(key,a){
     }
     return numberr[key]
 }
+function fun_snackbar(...text) {
+    var el = document.createElement("div");
+    el.className = "snackbar";
+    var y = document.getElementById("snackbar-container");
+    el.innerHTML = text;
+    y.append(el);
+    el.className = "snackbar show";
+    setTimeout(function(){ el.className = el.className.replace("snackbar show", "snackbar"); }, 3000);
+  }
